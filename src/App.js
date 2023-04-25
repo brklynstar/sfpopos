8b3e10733c1b4e01a3ef2b5cf989600b32b4f1f7
Brooke Finister <brookeafinister@gmail.com>
2023-04-25 14:26:51 -0700

    [UPDATE] content component built

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
+import POPOSList from './POPOSList';
 
 function App() {
   return (
     <div className="App">
       <Title />
-      <img src={logo} />
+      <POPOSList />
+
     </div>
   );
 }
+
 
 export default App;
